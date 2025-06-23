13785134a4b6919f34dcb534e27587637117af93
Hyunjoon Kim <wns41559@yonsei.ac.kr>
2025-06-23 05:25:29 +0000

    update Exqutor

--- PostgreSQL/pgvector/apply_patch.sh
+++ PostgreSQL/pgvector/apply_patch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Move to script directory (pgvector/)
-cd "$(dirname "$0")" || exit 1
+dir = "$(dirname "$0")"
+cd $(dir) || exit 1
 
 # Apply pgvector_Exqutor.patch to ./pgvector
 cd pgvector || exit 1
 git checkout v0.7.1
-patch -p1 < ../patch/pgvector_Exqutor.patch || exit 1
-cd ..
+patch -N -p1 < ../patch/pgvector_Exqutor.patch || exit 1
+
+cd $(dir)
 
 # Apply pgvector_Postgres.patch to ./postgres
 cd postgres || exit 1
