be856c943d94e1ba4719633d5269111f21d9d408
Hyunjoon Kim <wns41559@yonsei.ac.kr>
2025-06-24 04:52:48 +0000

    update Exqutor

--- PostgreSQL/VBASE/apply_patch.sh
+++ PostgreSQL/VBASE/apply_patch.sh
@@ -5,20 +5,21 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 cd "$dir" || exit 1
 
 cd MSVBASE/thirdparty/Postgres
-git apply ../../../patch/vbase_Postgres.patch || true
+git apply ../../../patch/vbase_Postgres.patch
 cd "$dir"
 
 cd MSVBASE/thirdparty/hnsw
-git apply ../../../patch/vbase_hnsw.patch || true
+git apply ../../../patch/vbase_hnsw.patch
 cd "$dir"
 
 
 cd MSVBASE/thirdparty/spann
-git apply ../../../patch/vbase_spann.patch || true
+git apply ../../../patch/vbase_spann.patch
 cd "$dir"
 
 cd MSVBASE
-git apply ../patch/vbase_Exqutor.patch || true
+git apply ../patch/vbase_Exqutor.patch
+git apply ../patch/vbase_Exqutor_lib.patch
 cd "$dir"
 
 
